9928516c41575dc539fcfe6d12eafbd59b7eaad3
Your name <you@example.com>
2023-11-02 10:36:42 +0500

    pdf default layout

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -80,70 +80,70 @@ function FileUpload() {
         <Paper elevation={3} style={{padding: 5}}>
             <Box p={3} textAlign="center">
                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
-                    <div
-                        className="rpv-core__viewer"
-                        style={{
-                            border: '1px solid rgba(0, 0, 0, 0.3)',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            height: '100%',
-                        }}
-                    >
-                        <div
-                            style={{
-                                alignItems: 'center',
-                                backgroundColor: '#eeeeee',
-                                borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-                                display: 'flex',
-                                padding: '4px',
-                            }}
-                        >
-                            <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>
-                        </div>
-                        <div
-                            style={{
-                                border: '1px solid rgba(0, 0, 0, .3)',
-                                display: 'flex',
-                                height: '100%',
-                                width: '100%',
-                            }}>
-
-                            <div
-                                style={{
-                                    flex: 1,
-                                    overflow: 'hidden',
-                                }}
-                            >
-                                <Viewer
-                                    fileUrl={pdfData as string}
-                                    plugins={[toolbarPluginInstance, searchPluginInstance]}
-                                    // scrollMode={ScrollMode.Horizontal}
-                                />
-                            </div>
-                            <div
-                                style={{
-                                    borderLeft: '1px solid rgba(0, 0, 0, .2)',
-                                    flex: '0 0 15rem',
-                                    width: '15rem',
-                                }}
-                            >
-                                <SearchSidebar searchPluginInstance={searchPluginInstance}/>
-                            </div>
-                        </div>
-                    </div>
                     {/*<div*/}
+                    {/*    className="rpv-core__viewer"*/}
                     {/*    style={{*/}
-                    {/*        height: '750px',*/}
-                    {/*        width: '900px',*/}
-                    {/*        marginLeft: 'auto',*/}
-                    {/*        marginRight: 'auto',*/}
+                    {/*        border: '1px solid rgba(0, 0, 0, 0.3)',*/}
+                    {/*        display: 'flex',*/}
+                    {/*        flexDirection: 'column',*/}
+                    {/*        height: '100%',*/}
                     {/*    }}*/}
                     {/*>*/}
-                    {/*    <Viewer*/}
-                    {/*        fileUrl={pdfData as string}*/}
-                    {/*        plugins={[defaultLayoutPluginInstance]}*/}
-                    {/*    />*/}
+                    {/*    <div*/}
+                    {/*        style={{*/}
+                    {/*            alignItems: 'center',*/}
+                    {/*            backgroundColor: '#eeeeee',*/}
+                    {/*            borderBottom: '1px solid rgba(0, 0, 0, 0.1)',*/}
+                    {/*            display: 'flex',*/}
+                    {/*            padding: '4px',*/}
+                    {/*        }}*/}
+                    {/*    >*/}
+                    {/*        <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>*/}
+                    {/*    </div>*/}
+                    {/*    <div*/}
+                    {/*        style={{*/}
+                    {/*            border: '1px solid rgba(0, 0, 0, .3)',*/}
+                    {/*            display: 'flex',*/}
+                    {/*            height: '100%',*/}
+                    {/*            width: '100%',*/}
+                    {/*        }}>*/}
+
+                    {/*        <div*/}
+                    {/*            style={{*/}
+                    {/*                flex: 1,*/}
+                    {/*                overflow: 'hidden',*/}
+                    {/*            }}*/}
+                    {/*        >*/}
+                    {/*            <Viewer*/}
+                    {/*                fileUrl={pdfData as string}*/}
+                    {/*                plugins={[toolbarPluginInstance, searchPluginInstance]}*/}
+                    {/*                // scrollMode={ScrollMode.Horizontal}*/}
+                    {/*            />*/}
+                    {/*        </div>*/}
+                    {/*        <div*/}
+                    {/*            style={{*/}
+                    {/*                borderLeft: '1px solid rgba(0, 0, 0, .2)',*/}
+                    {/*                flex: '0 0 15rem',*/}
+                    {/*                width: '15rem',*/}
+                    {/*            }}*/}
+                    {/*        >*/}
+                    {/*            <SearchSidebar searchPluginInstance={searchPluginInstance}/>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
                     {/*</div>*/}
+                    <div
+                        style={{
+                            height: '750px',
+                            width: '900px',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                        }}
+                    >
+                        <Viewer
+                            fileUrl={pdfData as string}
+                            plugins={[defaultLayoutPluginInstance]}
+                        />
+                    </div>
                 </Worker>
             </Box>
         </Paper>
